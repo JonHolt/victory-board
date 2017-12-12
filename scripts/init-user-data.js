39d97bb622bb171572a34d2dfe8f5e33d49740a6
JonHolt <jonathanholt92@gmail.com>
2017-12-11 18:41:42 -0700

    changed to new layout, started nav

---
+++ scripts/init-user-data.js
@@ -0,0 +1,23 @@
+initApp = function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var photoURL = user.photoURL;
+      user.getIdToken().then(function(accessToken) {
+        document.getElementById('profile-pic').src = user.photoURL || 'resources/default.jpg';
+        document.getElementById('profile-name').textContent = user.displayName;
+      });
+    } else {
+      // User is signed out.
+      document.getElementById('profile-pic').src = 'resources/default.jpg';
+      document.getElementById('profile-name').textContent = 'Sign In';
+    }
+  }, function(error) {
+    console.log(error);
+  });
+};
+
+window.addEventListener('load', function() {
+  initApp()
+});
