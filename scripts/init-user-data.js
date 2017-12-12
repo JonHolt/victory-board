2b3303f98ec1a80e52aae62d9a0474ed3c34ddca
JonHolt <jonathanholt92@gmail.com>
2017-12-11 20:06:57 -0700

    added remaining pages to the nav

--- scripts/init-user-data.js
+++ scripts/init-user-data.js
@@ -5,13 +5,13 @@ initApp = function() {
       var displayName = user.displayName;
       var photoURL = user.photoURL;
       user.getIdToken().then(function(accessToken) {
-        document.getElementById('profile-pic').src = user.photoURL || 'resources/default.jpg';
+        document.getElementById('profile-pic').src = user.photoURL || '/resources/default.jpg';
         document.getElementById('profile-name').textContent = user.displayName;
       });
     } else {
       // User is signed out.
-      document.getElementById('profile-pic').src = 'resources/default.jpg';
-      document.getElementById('profile-name').textContent = 'Sign In';
+      document.getElementById('profile-pic').src = '/resources/default.jpg';
+      document.getElementById('profile-name').textContent = 'Not Signed In';
     }
   }, function(error) {
     console.log(error);
