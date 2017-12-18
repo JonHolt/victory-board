c2e607e98bd5114da6fbff67f236e13fa3646df0
JonHolt <jonathanholt92@gmail.com>
2017-12-18 13:00:06 -0700

    profile page functioning

--- scripts/init-user-data.js
+++ scripts/init-user-data.js
@@ -1,3 +1,5 @@
+var userData;
+
 initApp = function() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -5,18 +7,37 @@ initApp = function() {
       var displayName = user.displayName;
       var photoURL = user.photoURL;
       user.getIdToken().then(function(accessToken) {
+        userData = {
+          photoURL: user.photoURL,
+          displayName: user.displayName,
+          uid: user.uid
+        };
+        
         document.getElementById('profile-pic').src = user.photoURL || '/resources/default.jpg';
         document.getElementById('profile-name').textContent = user.displayName;
+        // Tell page script to run
+        var event = new Event('JdoneLoading');
+        window.dispatchEvent(event);
       });
     } else {
       // User is signed out.
       document.getElementById('profile-pic').src = '/resources/default.jpg';
       document.getElementById('profile-name').textContent = 'Not Signed In';
+      delete userData;
+      // Tell page script to run
+      var event = new Event('JdoneLoading');
+      window.dispatchEvent(event);
     }
   }, function(error) {
     console.log(error);
   });
 };
+
+signOut = function() {
+  firebase.auth().signOut();
+}
+
+document.getElementById('logout-btn').onclick = signOut;
 
 window.addEventListener('load', function() {
   initApp()
---
+++ scripts/profile.js
@@ -0,0 +1,30 @@
+fillData = function() {
+  var winsElem = document.getElementById('wins'),
+      lossElem = document.getElementById('losses'),
+      pointsElem = document.getElementById('points'),
+      picElem = document.getElementById('profile-pic-big'),
+      nameElem = document.getElementById('name');
+  var hash = window.location.hash.substr(1);
+  var dbrefObj;
+  if (!hash) {
+    if (!userData) { 
+      return;
+    }
+    dbrefObj = firebase.database().ref().child('Players').child(userData.uid);
+  } else {
+    dbrefObj = firebase.database().ref().child('Players').child(hash);
+  }
+
+  dbrefObj.on('value', snap => {
+    var stats = snap.val();
+    picElem.src = stats.photoURL || '/resources/default.jpg';
+    nameElem.textContent = stats.name;
+    winsElem.textContent = stats.gamesWon;
+    lossElem.textContent = stats.gamesLost;
+    pointsElem.textContent = stats.points;
+  });
+};
+
+window.addEventListener('JdoneLoading', function(){
+  fillData();
+});
